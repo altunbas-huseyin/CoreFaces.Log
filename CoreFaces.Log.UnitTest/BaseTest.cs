ee1fc4b21a97d877b4a357998d6ea8ec1c01fd87
huseyin-altunbas <huseyin.altunbas@tutkal.com.tr>
2019-03-13 19:16:38 +0300

    Base Test Naming

--- CoreFaces.Log.UnitTest/BaseTest.cs
+++ CoreFaces.Log.UnitTest/BaseTest.cs
@@ -21,20 +21,21 @@ namespace CoreFaces.Log.UnitTest
         public BaseTest()
         {
 
-            DbContextOptionsBuilder<LogDatabaseContext> builder = new DbContextOptionsBuilder<LogDatabaseContext>();
+            DbContextOptionsBuilder<LogDatabaseContext> builderLog = new DbContextOptionsBuilder<LogDatabaseContext>();
             var connectionString = "server=localhost;userid=root;password=12345;database=Identity;";
-            builder.UseMySql(connectionString);
+            builderLog.UseMySql(connectionString);
 
 
-            _logDatabaseContext = new LogDatabaseContext(builder.Options);
+
+            _logDatabaseContext = new LogDatabaseContext(builderLog.Options);
 
             LogSettings _logSettings = new LogSettings() { FileUploadFolderPath = "c:/" };
-            IOptions<LogSettings> statusOptions = Options.Create(_logSettings);
+            IOptions<LogSettings> logOptions = Options.Create(_logSettings);
             IHttpContextAccessor iHttpContextAccessor = new HttpContextAccessor { HttpContext = new DefaultHttpContext() };
 
-            _logCategoryService = new LogCategoryService(_logDatabaseContext, statusOptions, iHttpContextAccessor);
-            _logService = new LogService(_logDatabaseContext, statusOptions, iHttpContextAccessor);
-            _logSchemaService=new LogSchemaService(_logDatabaseContext, statusOptions, iHttpContextAccessor);
+            _logCategoryService = new LogCategoryService(_logDatabaseContext, logOptions, iHttpContextAccessor);
+            _logService = new LogService(_logDatabaseContext, logOptions, iHttpContextAccessor);
+            _logSchemaService=new LogSchemaService(_logDatabaseContext, logOptions, iHttpContextAccessor);
         }
     }
 
